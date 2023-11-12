f648731a6f56e92fedd6c9be3cd68fe4065c2df9
Vít Novotný <witiko@mail.muni.cz>
2023-11-12 22:49:45 +0100

    Exit `Sustek-literate-rezani/run.sh` on a non-zero exit code

--- Sustek-literate-rezani/run.sh
+++ Sustek-literate-rezani/run.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+set -ex
 cd ukazky
 pdfcsplain zdrojaky
 pdfcsplain ukazky
 cd ..
 mpost graf
 mptopdf graf
-pdflatex -interaction=nonstopmode literate 
+pdflatex -interaction=nonstopmode literate || true
 pdflatex literate
 pdflatex literate
 pdflatex rezani
